dd6e6784e282f3dc3a910d1a0a1152c554350ff1
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-06 17:56:20 +0300

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 import './Home.css';
 
 const Home = () => {
@@ -10,7 +11,7 @@ const Home = () => {
 
     useEffect(() => {
         fetchDocuments();
-    }, []);
+    }, [username]);
 
     const fetchDocuments = async () => {
         try {
