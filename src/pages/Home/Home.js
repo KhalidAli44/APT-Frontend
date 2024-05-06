0dc3a448b82e17df3dbe69267fbd7f534d1a2633
MohabYasser2 <mohabhelp@gmail.com>
2024-05-06 17:50:46 +0300

    Mohab_1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,29 @@ import './Home.css';
 
 const Home = () => {
     const location = useLocation();
-    const username = new URLSearchParams(location.search).get('username');
+    const [documents, setDocuments] = React.useState([]);
+
+    // Function to add a new document
+    const addDocument = () => {
+        // Logic to create a new document and add it to the list of documents
+        const newDocument = {
+            id: Date.now(), // Generate a unique ID for the document
+            title: "Untitled Document",
+            content: ""
+        };
+        setDocuments([...documents, newDocument]);
+    };
 
     return (
-        <div><h1>Hi, {username}</h1></div>
+        <div className="home-container">
+            <h1>Welcome to the Text Editor</h1>
+            <button onClick={addDocument}>Add New Document</button>
+            <ul>
+                {documents.map((document) => (
+                    <li key={document.id}>{document.title}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
