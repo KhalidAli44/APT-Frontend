054738f1cec3b6621ba40499d13eb0c0eaedc8f0
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-12 04:13:53 +0300

    Sockets init

--- src/pages/TextEditor/TextEditor.js
+++ src/pages/TextEditor/TextEditor.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css'; 
 import './TextEditor.css';
 
 const TextEditor = () => {
-
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
 
@@ -12,9 +15,88 @@ const TextEditor = () => {
     const author = queryParams.get('author');
     const content = queryParams.get('content');
 
+    const editorRef = useRef(null);
+
+    const [stompClient, setStompClient] = useState([]);
+
+    useEffect(() => {
+        const socket = new SockJS('https://apt-backend.onrender.com/ws');
+        const client = Stomp.over(socket);
+
+        client.connect({}, () => {
+            client.subscribe(`/all/broadcast/${documentId}`, (message) => {
+                const receivedMessage = message.body;
+                console.log(receivedMessage);
+            });
+        });
+
+        setStompClient(client);
+
+        return () => {
+            //client.disconnect();
+        };
+
+    }, []);
+    
+    useEffect(() => {
+        if (!editorRef.current) {
+            editorRef.current = new Quill('#editor-container', {
+                modules: {
+                    toolbar: [
+                        ['bold', 'italic']
+                    ],
+                },
+                theme: 'snow'
+            });
+
+            editorRef.current.setText('');
+            editorRef.current.clipboard.dangerouslyPasteHTML(content);
+        }
+    }, [content]);
+
+    const handleSave = () => {
+        const newContent = editorRef.current.root.innerHTML;
+        
+        const documentObject = {
+            _id: documentId,
+            filename: filename,
+            author: author,
+            content: newContent
+        };
+
+        fetch(`https://apt-backend.onrender.com/documents/update/${documentId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(documentObject),
+        })
+        .then(response => {
+            if (response.ok) {
+                console.log('Document saved successfully.');
+            } else {
+                console.error('Failed to save document.');
+            }
+        })
+        .catch(error => {
+            console.error('Error saving document:', error);
+        });
+    };
+
+    const handleSendMessage = () => {
+        const message = editorRef.current.root.innerHTML;
+        stompClient.send(`/app/operation/${documentId}`, {}, message);
+    };
 
     return (
-        <div><h1>{filename}, By {author}</h1></div>
+        <div>
+            <div>
+                <h1>{filename}, By {author}</h1>
+                <button onClick={handleSave}>Save</button>
+            </div>
+            <div id="editor-container" className="editor-container" />
+            <button onClick={handleSendMessage}>Send</button>
+        </div>
     );
 };
 
