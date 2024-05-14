a96838bb9b1ad7ed54b4ee5f5a24a7ff9a29f6c5
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-15 00:56:19 +0300

    test

--- src/pages/TextEditor/TextEditor.js
+++ src/pages/TextEditor/TextEditor.js
@@ -151,6 +151,7 @@ const TextEditor = () => {
             console.log("text change: sessionId = " + sessionId);
             messages.push({ insertedIndex, insertedChar, sessionId });
             handleSendMessage(insertedIndex, insertedChar);
+            buffer = editorRef.current.getText();
         }
     };
 
