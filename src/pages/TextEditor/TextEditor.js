257d07740ff2beae4be9e7ce7d6be0eb489919df
MohabYasser2 <Mohabhelp@gmail.com>
2024-05-15 03:58:24 +0300

    aa

--- src/pages/TextEditor/TextEditor.js
+++ src/pages/TextEditor/TextEditor.js
@@ -172,19 +172,24 @@ const TextEditor = () => {
     {
         
         console.log(" insertAtIndex: CHar = " + character );
-        if (SpaceFlag === true && character !== '\n') 
+        // if (SpaceFlag === true && character !== '\n') 
+        //     {
+        //         index = index + 1;
+        //         SpaceFlag = false; 
+        //     }
+        //     if (character === '\n'&& SpaceFlag === true) 
+        //         {
+        //             index = index - 1;
+        //         }
+        // if (character === '\n') 
+        // {
+        //     SpaceFlag = true;
+        // }
+
+        if(buffer[index-1] === '\n')
             {
                 index = index + 1;
-                SpaceFlag = false; 
             }
-            if (character === '\n'&& SpaceFlag === true) 
-                {
-                    index = index - 1;
-                }
-        if (character === '\n') 
-        {
-            SpaceFlag = true;
-        }
         
         buffer = buffer.substring(0, index) + character + buffer.substring(index);
 
